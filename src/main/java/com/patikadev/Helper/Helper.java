606d8ddc81952803167b4f5bc04866092044b48e
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-06-09 14:08:15 +0300

    Initial Create

--- src/main/java/com/patikadev/Helper/Helper.java
+++ src/main/java/com/patikadev/Helper/Helper.java
@@ -1,2 +1,34 @@
-package com.patikadev.Helper;public class Helper {
+package com.patikadev.Helper;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Helper {
+    public static void setLayout(){
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()){
+            if ("Nimbus".equals(info.getName())){
+                try {
+                    UIManager.setLookAndFeel(info.getClassName());
+                } catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException |
+                         InstantiationException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            }
+        }
+    }
+    public static int scCenter(String eksen, Dimension size){
+        int point;
+        switch (eksen){
+            case "x" :
+                point = (Toolkit.getDefaultToolkit().getScreenSize().width - size.width) / 2;
+                break;
+            case "y" :
+                point = (Toolkit.getDefaultToolkit().getScreenSize().height - size.height) / 2;
+                break;
+            default:
+                point = 0;
+        }
+        return point;
+    }
 }
--- src/main/java/com/patikadev/View/OperatorGUI.java
+++ src/main/java/com/patikadev/View/OperatorGUI.java
@@ -1,2 +1,33 @@
-package com.patikadev.View;public class OperatorGUI {
+package com.patikadev.View;
+import com.patikadev.Helper.*;
+import com.patikadev.Model.Operator;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class OperatorGUI extends JFrame {
+    private JPanel wrapper;
+
+    private final Operator operator;
+
+    public OperatorGUI(Operator operator){
+        this.operator = operator;
+        Helper.setLayout();
+        add(wrapper);
+        setSize(1000, 500);
+        setLocation(Helper.scCenter("x", getSize()), Helper.scCenter("y", getSize()));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle(Config.PROJECT_TITLE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Operator op = new Operator();
+        op.setId(1);
+        op.setName("Mert Taş");
+        op.setUname("mertcan");
+        op.setPass("taskiran");
+        op.setType("Operator");
+        OperatorGUI opGUI = new OperatorGUI(op);
+    }
 }
